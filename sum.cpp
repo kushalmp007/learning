28aa9fcfd31072ac4a8728aed426f1aad4434009
Kushal <kushalmp26072003@gmail.com>
2022-08-26 11:47:30 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+                    
+int main()
+                    
+{
+// #ifndef ONLINE_JUDGE
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+// #endif
+ int a=2,b=3,c;
+ c= a+b;
+ cout<<c<<endl;
+ return 0;
+}
